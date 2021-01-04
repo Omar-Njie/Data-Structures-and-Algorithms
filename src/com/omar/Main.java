bf23b976a3a279663ebdd500d7113299c1e76391
El Hadji Omar Ndiaye <66810439+Zinhox@users.noreply.github.com>
2021-01-04 11:44:19 -0800

    first commit

---
+++ src/com/omar/Main.java
@@ -0,0 +1,24 @@
+package com.omar;
+import java.util.*;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+	// write your code here
+        //What is an Algorithm
+        //An algorithm is a way of solving well specified computational problems.
+        //A finite of rules that give a sequence of operations for solving a specific type pf problem.
+
+       /*for (int i = 0; 1 < A.length-1; i++) {
+           for (int j = 0; j < A.length-1-i; j++ ) {
+               if (A[j] > A[j+1]) {
+
+               }
+           }
+       }*/
+
+        int[] numbers = new int[3];
+        System.out.println(Arrays.toString(numbers));
+    }
+}
